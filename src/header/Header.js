94056da1bb743170db4ea46f42260a78065aca4e
Dave Trost <davetrost@gmail.com>
2020-04-22 15:53:11 -0700

    semantic cleanup for header

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,14 +2,15 @@ import React from 'react';
 import stellaLogo from '../assets/STELLA LOGO 1.png';
 import phoneImg from '../assets/Phone.png';
 import './Header.scss';
+
 function Header() {
   return (
     <header  className='Header'>
       <div className='stellaLogo'>
         <img src={stellaLogo} alt='stella logo'></img>
       </div>
-      <button className='stellaPhone'>
-        <label>
+      <button className='stellaPhone' id='stellaPhone'>
+        <label htmlFor='stellaPhone'>
           <img src={phoneImg} alt='phone'></img>
           888-781-7074
         </label>
