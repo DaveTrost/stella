8bf25c4bd898cf3587f33e74a23c2fe444f44488
Dave Trost <davetrost@gmail.com>
2020-04-22 11:36:22 -0700

    Header component and styling

---
+++ src/header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import stellaLogo from '../assets/STELLA LOGO 1.png';
+import phoneImg from '../assets/Phone.png';
+import './Header.scss';
+function Header() {
+  return (
+    <header  className='Header'>
+      <div className='stellaLogo'>
+        <img src={stellaLogo} alt='stella logo'></img>
+      </div>
+      <button className='stellaPhone'>
+        <label>
+          <img src={phoneImg} alt='phone'></img>
+          888-781-7074
+        </label>
+      </button>
+    </header>
+  );
+}
+
+export default Header;
