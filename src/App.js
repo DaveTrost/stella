3f82e915284ee52bd311f076fdbe80e493efac1c
Dave Trost <davetrost@gmail.com>
2020-04-23 22:59:09 -0700

    testing at App level

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Header from './header/Header';
+import Header from './components/Header/Header';
+import Calculate from './containers/Calculate/Calculate';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Calculate />
+      <Footer />
     </div>
   );
 }
