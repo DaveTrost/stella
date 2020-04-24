c03f941f828f76a4ce108a5fa43abc7b0ceca01a
Dave Trost <davetrost@gmail.com>
2020-04-23 22:57:55 -0700

    integrate Message and Progress components.

--- src/containers/Calculate/Calculate.js
+++ src/containers/Calculate/Calculate.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import StellaSez from '../../StellaSez/StellaSez';
+import ProgressBar from '../../components/ProgressBar/ProgressBar';
+import StellaSez from '../../components/StellaSez/StellaSez';
+import Message from '../../components/StellaSez/Message';
 import './Calculate.scss';
 
 function Calculate({ avatar, children }) {
   return (
     <div className='Calculate'>
+      <ProgressBar now={66} />
       <StellaSez avatar='https://stella-dev.demand-iq.com/media/avatars/65.jpg'>
-        <p>
+        <Message text={`
           Almost done! 
           I just need your most recent monthly electric bill 
           to calculate your savings
-        </p>
+        `} />
+      </StellaSez>
+      <StellaSez avatar='https://stella-dev.demand-iq.com/media/avatars/65.jpg'>
+        <Message text={`
+          Hang on. I am calculating your savings...
+        `} />
+      </StellaSez>
+      <StellaSez avatar='https://stella-dev.demand-iq.com/media/avatars/65.jpg'>
+        <Message text={`
+          Sizing your solar system...
+        `} />
+      </StellaSez>
+      <StellaSez avatar='https://stella-dev.demand-iq.com/media/avatars/65.jpg'>
+        <Message text={`
+          Checking on energy prices...
+        `} />
+      </StellaSez>
+      <StellaSez avatar='https://stella-dev.demand-iq.com/media/avatars/65.jpg'>
+        <Message text={`
+          Let me know when you're ready to see pricing!
+        `} />
       </StellaSez>
     </div>
   );
