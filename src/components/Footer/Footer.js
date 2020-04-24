2bc437c6c3fac31b5ddddfc58aa905e6853528a7
Dave Trost <davetrost@gmail.com>
2020-04-23 20:47:37 -0700

    Move footer into components folder. some styling tweaks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import fiveStarsImg from '../assets/five-stars.png';
-import lockImg from '../assets/lock.png';
+import fiveStarsImg from '../../assets/five-stars.png';
+import lockImg from '../../assets/lock.png';
 import './Footer.scss';
 
 function Footer() {
@@ -10,7 +10,7 @@ function Footer() {
         {`Our customers say`}
         <em>Excellent</em>
         <img src={fiveStarsImg} alt='5-star-rating'></img>
-        <sub>4.97/5</sub>
+        <span className='smaller'>4.97/5</span>
       </p>
       <p className='privacy'>
         <img src={lockImg} alt='secure'></img>
