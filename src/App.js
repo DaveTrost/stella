baaa574c121795ddb5d0a63230794ca8ba7b9434
Dave Trost <davetrost@gmail.com>
2020-04-23 22:59:39 -0700

    styling updates for responsive-ness

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Calculate from './containers/Calculate/Calculate';
 import Footer from './components/Footer/Footer';
+import './App.scss';
 
 function App() {
   return (
