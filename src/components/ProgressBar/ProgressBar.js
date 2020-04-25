42bf96673dfc29f264d83f79431004cdfe11ca57
Dave Trost <davetrost@gmail.com>
2020-04-24 23:26:10 -0700

    cleanup - declare all components consistently w/ "function"

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './ProgressBar.scss';
 
-const ProgressBar = ({ now }) => (
-  <div className='ProgressBar'>
-    <span className='readout' style={{ width: `${now}%` }} >{now}%</span>
-    <div className='progress'>
-      <div className='fill' style={{ width: `${now}%` }} />
+function ProgressBar({ now }) {
+  return (
+    <div className='ProgressBar'>
+      <span className='readout' style={{ width: `${now}%` }} >{now}%</span>
+      <div className='progress'>
+        <div className='fill' style={{ width: `${now}%` }} />
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default ProgressBar;
