7664140295c9c152038c6875d37d37a41008e77c
Dave Trost <davetrost@gmail.com>
2020-04-25 01:42:05 -0700

    ... on second thought, remove testing.
    Not a good move for a professional project!
    Hopefuly it is acceptable for this interview challenge

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const footerRating = getByText(/Excellent/i);
-  expect(footerRating).toBeInTheDocument();
-});
