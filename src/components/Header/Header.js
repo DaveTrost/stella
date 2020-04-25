5eba4715a61cae76e53384e3493ebf1513ee008b
Dave Trost <davetrost@gmail.com>
2020-04-24 23:54:36 -0700

    Add start over button to header, controlled by props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Button from '../Button/Button';
 import stellaLogo from '../../assets/STELLA LOGO 1.png';
+import startOver from '../../assets/startOver.png';
 import phoneImg from '../../assets/Phone.png';
 import './Header.scss';
 
-function Header() {
+function Header({ showStartOver }) {
   return (
-    <header  className='Header'>
+    <header className='Header'>
       <div className='stellaLogo'>
         <img src={stellaLogo} alt='stella logo'></img>
+      </div>
+      <div className={`startOver ${!showStartOver && 'nodisplay'}`}>
+        <img src={startOver} alt='start over'></img>
       </div>
       <div className='stellaPhone'>
         <Button
