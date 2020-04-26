45566df376dd013a2a90ff2cf4e5f8ff9b36a81d
Dave Trost <davetrost@gmail.com>
2020-04-26 14:28:00 -0700

    fetch data from user progresses into state

--- src/store/userProgress/actions.js
+++ src/store/userProgress/actions.js
@@ -1,3 +1,33 @@
+import { createAction } from 'promise-middleware-redux';
+import { getProgress } from '../../services/demand-iq/demandIqApi';
+
+export const [
+  fetchUserDataFromApi,
+  FETCH_USER_DATA,
+  FETCH_USER_DATA_LOADING,
+  FETCH_USER_DATA_DONE,
+  FETCH_USER_DATA_REJECTED
+] = createAction('FETCH_USER_DATA', () => {
+  return getProgress('http://localhost:3000')
+    .then(async (res) => {
+      const body = await res.json();
+      
+      const isNewUser = /not found/i.test(body.detail);
+      if(!res.ok && isNewUser) {
+        console.log(body);
+        return body;
+      }
+      
+      if(!res.ok) {
+        /* Note: FETCH_USER_DATA_REJECTED is unreachable unless the promise rejects here ...
+          see if this can be done later */
+        throw res.error;
+      }
+
+      return body;
+    });
+});
+
 export const RESET_USER_DATA = 'RESET_USER_DATA';
 export const resetUserData = () => ({ type: RESET_USER_DATA });
 
--- src/store/userProgress/reducer.js
+++ src/store/userProgress/reducer.js
@@ -1,12 +1,22 @@
 import Immutable from 'seamless-immutable';
-import { SET_USER_DATA, SET_STEP, RESET_USER_DATA } from './actions';
+import { 
+  SET_USER_DATA, 
+  SET_STEP, 
+  RESET_USER_DATA, 
+  FETCH_USER_DATA, 
+  FETCH_USER_DATA_LOADING, 
+  FETCH_USER_DATA_DONE, 
+  FETCH_USER_DATA_REJECTED
+} from './actions';
 
 export const UNKNOWN = '';
+export const ERROR = 'error';
 export const CALCULATE1 = '/calculate2';
 export const CALCULATE2 = '/calculate3';
 
 const initialState = Immutable({
   loading: true,
+  error: '',
   step: UNKNOWN,
   userData: {
     avgBill: '250',
@@ -15,6 +25,20 @@ const initialState = Immutable({
 
 export default function reduce(state = initialState, action = {}) {
   switch (action.type) {
+    case FETCH_USER_DATA_LOADING:
+      return state.merge({loading: true});
+    case FETCH_USER_DATA_DONE:
+      return state.merge({loading: false});
+    case FETCH_USER_DATA_REJECTED:
+      return state.merge({
+        error: action.payload,
+        step: ERROR
+      });
+    case FETCH_USER_DATA:
+      return state.merge({
+        userData: action.payload,
+        step: action.payload.title,
+      });
     case RESET_USER_DATA:
       return initialState;
     case SET_USER_DATA:
