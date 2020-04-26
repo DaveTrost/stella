5178010aa3a182750a54d9b7a940847414c62156
Dave Trost <davetrost@gmail.com>
2020-04-26 08:42:32 -0700

    API calls for the solarcompanies and progresses endpoints

---
+++ src/services/demand-iq/demandIqApi.js
@@ -0,0 +1,21 @@
+import { get, post, patch } from '../request';
+
+const header = {
+  'APP-URL': 'https://estimate.sunlytix.com/',
+};
+
+const PROGRESSES_URL = 'https://cors-anywhere.herokuapp.com/https://stella-dev.demand-iq.com/api/progresses/me/';
+export const getProgress = (appUrl2 = '') => {
+  return get(PROGRESSES_URL, { ...header, 'APP-URL-2': appUrl2 });
+};
+export const postProgress = (appUrl2 = '', body) => {
+  return post(PROGRESSES_URL, { ...header, 'APP-URL-2': appUrl2 }, body);
+};
+export const patchProgress = (appUrl2 = '', body) => {
+  return patch(PROGRESSES_URL, { ...header, 'APP-URL-2': appUrl2 }, body);
+};
+
+const SOLARCOMPANIES_URL = 'https://cors-anywhere.herokuapp.com/https://stella-dev.demand-iq.com/api/solarcompanies/me/';
+export const getSolarCompany = (appUrl2 = '') => {
+  return get(SOLARCOMPANIES_URL, { ...header, 'APP-URL-2': appUrl2 });
+};
