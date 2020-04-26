fcae4bb9f4333c1db34446b7118087318a2b3ebd
Dave Trost <davetrost@gmail.com>
2020-04-26 14:15:36 -0700

    insert proxy into URL's as needed

--- src/services/demand-iq/demandIqApi.js
+++ src/services/demand-iq/demandIqApi.js
@@ -4,7 +4,10 @@ const header = {
   'APP-URL': 'https://estimate.sunlytix.com/',
 };
 
-const PROGRESSES_URL = 'https://cors-anywhere.herokuapp.com/https://stella-dev.demand-iq.com/api/progresses/me/';
+// for development only, set to empty string for production
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const BASE_URL = 'https://stella-dev.demand-iq.com/api/';
+const PROGRESSES_URL = `${PROXY_URL}${BASE_URL}progresses/me/`;
 export const getProgress = (appUrl2 = '') => {
   return get(PROGRESSES_URL, { ...header, 'APP-URL-2': appUrl2 });
 };
@@ -15,7 +18,7 @@ export const patchProgress = (appUrl2 = '', body) => {
   return patch(PROGRESSES_URL, { ...header, 'APP-URL-2': appUrl2 }, body);
 };
 
-const SOLARCOMPANIES_URL = 'https://cors-anywhere.herokuapp.com/https://stella-dev.demand-iq.com/api/solarcompanies/me/';
+const SOLARCOMPANIES_URL = `${BASE_URL}solarcompanies/me/`;
 export const getSolarCompany = (appUrl2 = '') => {
   return get(SOLARCOMPANIES_URL, { ...header, 'APP-URL-2': appUrl2 });
 };
