e5ff5a937dd1548ad085d433c4a92a2d11d28d48
Dave Trost <davetrost@gmail.com>
2020-04-26 12:06:28 -0700

    fix app so it renders even when a "step" is unknown

--- src/containers/Calculate/stellaMessages.js
+++ src/containers/Calculate/stellaMessages.js
@@ -1,16 +1,19 @@
-import { CALCULATE1, CALCULATE2, UNKNOWN } from '../../store/userProgress/reducer';
+import { CALCULATE1, CALCULATE2 } from '../../store/userProgress/reducer';
 
-const messageLookup = {
-  [UNKNOWN]: [],
-  [CALCULATE1]: [
-    `Almost done! I just need your most recent 
-    monthly electric bill to calculate your savings`,
-  ],
-  [CALCULATE2]: [
-    `Hang on. I am calculating your savings...`,
-    `Sizing your solar system...`,
-    `Checking on energy prices...`,
-  ],
-};
-
-export const getStellaMessages = step => messageLookup[step];
+export const getStellaMessages = step => {
+  switch(step) {
+    case CALCULATE1:
+      return [
+        `Almost done! I just need your most recent 
+        monthly electric bill to calculate your savings`,
+      ];
+    case CALCULATE2: 
+      return [
+        `Hang on. I am calculating your savings...`,
+        `Sizing your solar system...`,
+        `Checking on energy prices...`,
+      ];
+    default:
+      return [];
+  }    
+}
