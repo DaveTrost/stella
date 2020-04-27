bb48998d9551b9536ebcb9d0d829d34b11e935ca
Dave Trost <davetrost@gmail.com>
2020-04-26 21:04:59 -0700

    message list needs to be sensitive to loading condition

--- src/containers/Calculate/stellaMessages.js
+++ src/containers/Calculate/stellaMessages.js
@@ -1,12 +1,14 @@
-import { CALCULATE1, CALCULATE2 } from '../../store/userProgress/reducer';
+import { CALCULATE1, CALCULATE2, NEW_USER } from '../../store/userProgress/reducer';
 
-export const getStellaMessages = step => {
+export const getStellaMessages = (step, loading) => {
   switch(step) {
     case CALCULATE1:
-      return [
-        `Almost done! I just need your most recent 
-        monthly electric bill to calculate your savings`,
-      ];
+    case NEW_USER:
+      return loading 
+        ? [] 
+        : [`Almost done! I just need your most recent 
+          monthly electric bill to calculate your savings`,
+        ];
     case CALCULATE2: 
       return [
         `Hang on. I am calculating your savings...`,
